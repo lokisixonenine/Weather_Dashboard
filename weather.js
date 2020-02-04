58e00e0f9e1f4737a100ed21698c0e81b6abdc4d
Jaycee Bagtas <jaycee.bagtas@gmail.com>
2020-02-03 19:08:42 -0800

    working on JS file.

--- weather.js
+++ weather.js
@@ -1,44 +1,44 @@
-const apiKey ="527c3e91b03d76d0c938d2a15e7af232";
-let searched; 
+const apiKey = "527c3e91b03d76d0c938d2a15e7af232";
+let searched;
 
-updateSearch =()=>{
-    searched = json.parse(localStorage.getItem('searchHistory')) || [];
-    $('#searchHistory').html('');
-    searched.forEach(item=>$('#searchHistory').append('<button class='btn'
-    style='border-width: 1px; border-color: gray; margin:auto; width: 100%;'>${item} 
-    </button>'))
+updateSearch = () => {
+    searched = JSON.parse(localStorage.getItem('searchHistory')) || [];
+    $('#searchHistory').html('')
+    searched.forEach(item => $('#searchHistory').append(`<button class='btn' 
+    style='border-width:1px; border-color:black; margin:auto; width:100%'>${item}</button>`))
 }
-updateSearch()
 
-handleSearch = (val) =>{
-    $ajax({
+
+handleSearch = (val) => {
+    $.ajax({
         method: "GET",
-        url: 'https://api.openweathermap.org/data/2.5/forecast?q=${val}&{appKey}'
-    }).then(data=>{
+        url: `https://api.openweathermap.org/data/2.5/forecast?q=${val}&appid=${apiKey}`
+    }).then(data => {
         searched.push(val)
         localStorage.setItem('searchHistory', JSON.stringify(searched))
         updateSearch()
-        $('.col-8').prePend('<div class="Row"><h3>5 Day Forecast:</h3></div>')
-        data.list.forEach((datum, i)=> {
-            (!((i+4)%8)) ?
-                $('#results').append('<div class="card col-2" style="width: 8rem;">'
-                <img class ="card-imag-top" src="..." alt="Card Image Cap">
-                    <div class="card-body">
-                        <h5 class="card-title">Card titel</h5>
-                        <p class="card=text">Some quick text to build on the card and make up the bulk of the content.</p>
-                        <a href="#" class="btn btn-primary">Go Somewhere</a>
-                    </div>
-                </div>')
-                : ''
-            })
-        }).catch(err=> console.log(err))
+        $('.col-8').prepend('<div class="row"><h3>5 Day Forecast:</h3></div>')
+        data.list.forEach((datum, i) => {
+            (!((i + 4) % 8)) ?
+            $('#results').append(`<div class="card col-2" style="width: 8rem;">
+        <img class="card-img-top" src="..." alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>`): ''
+        })
+    }).catch(err => console.log(err))
+
 }
-$('btn').on('click', funciton () {
+$('.btn').on('click', function () {
     console.log($(this).text())
-    if($(this).text()==="Search"){
+    if ($(this).text() === "Search") {
         let city = $('#search').val().trim()
         handleSearch(city)
-    }else{
+    } else {
         handleSearch($(this).text())
     }
+
 })
